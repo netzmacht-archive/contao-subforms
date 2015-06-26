c3d3bd574fba040929373bbe029a3e9b1a0e19d3
David Molineus <david.molineus@netzmacht.de>
2015-06-26 08:37:36 +0200

    Add german translation.

---
+++ module/languages/de/tl_form_field.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+/*
+ * Form fields
+ */
+$GLOBALS['TL_LANG']['FFL']['subform'][0] = 'Unterformular';
+$GLOBALS['TL_LANG']['FFL']['subform'][1] = 'Felder eines anderen Formulars übernehmen.';
+
+/*
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['subform_legend'] = 'Unterformular';
+
+/*
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['subform'][0]                   = 'Unterformular';
+$GLOBALS['TL_LANG']['tl_form_field']['subform'][1]                   = 'Bitte ein Unterformular auswählen, dessen Felder übernommen werden sollen.';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStore'][0]          = 'Anfrage speichern';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStore'][1]          = 'Bitte wählen Sie die Formularfelder aus, die in der Anfrage gespeichert weden sollen.';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStoreField'][0]     = 'Formularfelder';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStoreField'][1]     = 'Bitte Feld auswählen.';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStoreLeadStore'][0] = 'In Anfrage speichern';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStoreLeadStore'][1] = 'Bitte wählen Sie aus ob bzw. unter welchem namen die Formulareingabe gespeichert weden soll';
+$GLOBALS['TL_LANG']['tl_form_field']['subformLeadStoreLeadStore'][2] = 'nicht speichern';
