08c586b65ab5c4b98402a32e106ef29de53a517a
David Molineus <david.molineus@netzmacht.de>
2015-06-26 08:39:17 +0200

    Add module translation.

---
+++ module/languages/de/modules.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['MOD']['subform'][0] = 'Unterformulare';
+$GLOBALS['TL_LANG']['MOD']['subform'][1] = 'Große Formulare mit Unterformularen verwalten';
---
+++ module/languages/en/modules.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['MOD']['subform'][0] = 'Subform';
+$GLOBALS['TL_LANG']['MOD']['subform'][1] = 'Manage large forms with subforms.';
